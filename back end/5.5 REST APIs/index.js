fcd57aa233ca2028e05c2c14c6c7d0dcf18a36ec
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-06-10 22:26:44 -0400

    make put method able to replace a resource on the server with the new data that the user sends

--- back end/5.5 REST APIs/index.js
+++ back end/5.5 REST APIs/index.js
@@ -13,7 +13,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 
 //TODO 1: Add your own bearer token from the previous lesson.
 const yourBearerToken = "6986d151-66ca-470d-946b-445150eea483";
-const config = {
+const config = { //CRIS/ we'll create the config object that has the token authentication
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
 
@@ -37,12 +37,6 @@ app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
   const userData = req.body; //CRIS/ we'll collect the answers from the user here
 
-  const config = { //CRIS/ we'll create the config object that has the token authentication
-    headers: {
-      Authorization: `Bearer ${yourBearerToken}`
-    }
-  };
-
   try { //CRIS/ this try catch block should try to connect to the external server and to create a new resource using the user's data
     const result = await axios.post(API_URL + "/secrets", userData, config)
     res.render("index.ejs", { content: JSON.stringify(result.data) });
@@ -54,6 +48,14 @@ app.post("/post-secret", async (req, res) => {
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  const userData = req.body;
+
+  try {
+    const result = await axios.put(API_URL + "/secrets/" + searchId, userData, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: error.response.data });
+  }
 });
 
 app.post("/patch-secret", async (req, res) => {
@@ -64,12 +66,6 @@ app.post("/patch-secret", async (req, res) => {
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${yourBearerToken}`
-    }
-  };
 
   try {
     const result = await axios.delete(API_URL + "/secrets/" + searchId, config);
