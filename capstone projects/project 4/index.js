ee7181bf4c30dab3e95db6c32f177afea4de8452
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-06-13 20:25:54 -0400

    add axios.get for the other buttons, use json.stringify to show the data that we get from the server

--- capstone projects/project 4/index.js
+++ capstone projects/project 4/index.js
@@ -12,15 +12,56 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-app.post("/link", async (req, res) => {
-    const id = "0100000004180902";
+
+//if the user clicks on "Link" the character from "Zelda Tears of the Kingdom"
+app.post("/link", async (req, res) => { //we use async and await to make sure the app doesn't freeze when we're trying to get the data from the external server
+    const id = "0100000004180902"; //we'll attach this to the URL to get the exact amiibo from the website
+
+    try { //will use axios to communicate with external server
+        const result = await axios.get(URL + id);
+
+        res.render("index.ejs", { //this will generate an html file with the data that we pass it
+            name: result.data.amiibo.name,
+            image: result.data.amiibo.image,
+            info: JSON.stringify(result.data)
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Something went wrong");
+    }
+
+});
+
+//if the user clicks on zelda
+app.post("/zelda", async (req, res) => {
+    const id = "0101000004190902";
 
     try {
         const result = await axios.get(URL + id);
 
         res.render("index.ejs", {
-            name: result.data.amiibo.character,
-            image: result.data.amiibo.image
+            name: result.data.amiibo.name,
+            image: result.data.amiibo.image,
+            info: JSON.stringify(result.data)
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Something went wrong");
+    }
+
+});
+
+//if the user clicks on samus
+app.post("/samus", async (req, res) => {
+    const id = "05C0000004121302";
+
+    try {
+        const result = await axios.get(URL + id);
+
+        res.render("index.ejs", {
+            name: result.data.amiibo.name,
+            image: result.data.amiibo.image,
+            info: JSON.stringify(result.data)
         });
     } catch (error) {
         console.log(error.message);
