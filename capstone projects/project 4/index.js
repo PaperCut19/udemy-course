8edd558533c6c4f82cc4eed3c38305df1e2cdfbb
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-06-13 20:09:52 -0400

    use axios to communicate with external server (API provider) when user clicks on one of the buttons

--- capstone projects/project 4/index.js
+++ capstone projects/project 4/index.js
@@ -4,8 +4,29 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+const URL = "https://amiiboapi.com/api/amiibo/?id=";
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.post("/link", async (req, res) => {
+    const id = "0100000004180902";
+
+    try {
+        const result = await axios.get(URL + id);
+
+        res.render("index.ejs", {
+            name: result.data.amiibo.character,
+            image: result.data.amiibo.image
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Something went wrong");
+    }
+
 });
 
 app.listen(port, () => {
