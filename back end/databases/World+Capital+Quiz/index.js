755862a59083ea014098db6bf2d24741d10b163a
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-06-22 16:32:44 -0400

    add documentation to world quiz folder

--- back end/databases/World+Capital+Quiz/index.js
+++ back end/databases/World+Capital+Quiz/index.js
@@ -23,28 +23,28 @@ app.get("/", async (req, res) => {
   totalCorrect = 0;
   await nextQuestion();
   console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
+  res.render("index.ejs", { question: currentQuestion }); //CRIS/ when the home page is requested, we'll send the index.js file with the current question
 });
 
 // POST a new post
 app.post("/submit", (req, res) => {
-  let answer = req.body.answer.trim();
+  let answer = req.body.answer.trim(); //CRIS/ user data, we use trim to remove the spaces before and after
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) { //CRIS/ comparing the data to the answer
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
   }
 
-  nextQuestion();
-  res.render("index.ejs", {
+  nextQuestion(); //CRIS/ creating the new question
+  res.render("index.ejs", { //CRIS/ sending index.js file with the new question and updated score/counter
     question: currentQuestion,
     wasCorrect: isCorrect,
     totalScore: totalCorrect,
   });
 });
 
-async function nextQuestion() {
+async function nextQuestion() { //CRIS/ creating a function that can pick a random country
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
 
   currentQuestion = randomCountry;
