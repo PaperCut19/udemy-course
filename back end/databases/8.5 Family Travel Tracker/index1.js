a7c3e73275280ab6d957492992a6057fbeb77df0
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-07-02 13:33:54 -0400

    add middleware and database object

--- back end/databases/8.5 Family Travel Tracker/index1.js
+++ back end/databases/8.5 Family Travel Tracker/index1.js
@@ -5,6 +5,18 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "gyccof-rajwy0-pyccAx",
+    port: 5432
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 })
